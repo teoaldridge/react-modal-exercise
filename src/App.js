341d16f5a82d5375124c1af12400bd5ef9f1442c
Teodora Aldridge <74827533+teoaldridge@users.noreply.github.com>
2022-05-04 14:09:20 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from './components/Todo'
+
+function App() {
+  return <div>
+    <h1>My Todos</h1>
+    <Todo text='Invite friends to the party'/>
+    <Todo text='Organise the party'/>
+    <Todo text='Shop for the party.'/>
+    </div>;
+
+}
+
+export default App;
